997b8355cfa6af7a5e75bf569ab4fb903f6f2e35
Vaibhav Mehar <66452495+Vaib19121@users.noreply.github.com>
2021-10-20 11:16:25 +0530

    updated changes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,3 @@
-/*
- * Here's a challenge that's a bit more mathematical in nature.
- * Write a program that determines the number of trailing zeros at the end of X! (X factorial),
- * where X is an arbitrary number.
- * For instance, 5! is 120, so it has one trailing zero.
- */
 
 #include <iostream>
 
@@ -19,14 +13,13 @@ int main()
     while(num>0)
     {
         num = num/5;
-        count5++;               //count the number of times 5 divides the number
+        count5++;               
         if(num>0)
         {
-            num = num/2;        //count the number of times 2 divides the number
+            num = num/2;      
             count2++;
         }
     }
-    //together number is divisible by 10 or 5(as 5! == 120)
     cout<<"\nNo. of zeros in factorial are "<<count5+count2;
     return 0;
 }
